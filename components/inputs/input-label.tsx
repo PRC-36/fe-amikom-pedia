ab1213924b6dceaf74381e1c91895c1afd4efa0d
Muhammad Nizar Fazari <nexsel32@gmail.com>
2024-02-02 20:52:44 +0700

    added react hook form in register form

--- components/inputs/input-label.tsx
+++ components/inputs/input-label.tsx
@@ -1,19 +1,19 @@
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-
+import React, { forwardRef } from 'react';
 interface IInputLabelProps {
-    name?: string,
+    names?: string,
     type?: string,
     placeholder?: string
 }
 
-const InputLabel: React.FunctionComponent<IInputLabelProps> = ({ name = 'Nama', type = 'text', placeholder = 'Nama..' }) => {
+const InputLabel: React.ForwardRefRenderFunction<HTMLInputElement ,IInputLabelProps> = ({ names = 'Nama', type = 'text', placeholder = 'Nama..', ...props }, ref) => {
     return (
         <div className="grid w-full max-w-sm items-center gap-2">
-            <Label htmlFor={name} className='text-base font-semibold'>{name}</Label>
-            <Input type={type} id={name} placeholder={placeholder} className='text-text-500 ' />
+            <Label htmlFor={names} className='text-base font-semibold'>{names}</Label>
+            <Input ref={ref} type={type} id={names} placeholder={placeholder} className='text-text-500 ' autoComplete={names} {...props} />
         </div>
     );
 };
 
-export default InputLabel;
+export default forwardRef(InputLabel);
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,11 +1,15 @@
 
 import InputLabel from '@/components/inputs/input-label';
 import * as React from 'react';
-
+import { useForm } from "react-hook-form";
 interface IRegisterProps {
 }
 
 const Register: React.FunctionComponent<IRegisterProps> = (props) => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data : any) => console.log(data);
+
+
     return (
         <>
             <div className='grid grid-cols-1 lg:grid-cols-2 h-screen'>
@@ -21,12 +25,12 @@ const Register: React.FunctionComponent<IRegisterProps> = (props) => {
                             <h2 className='text-2xl font-semibold text-text-500 mb-2'>Masuk ke Akun anda</h2>
                             <p className='text-base font-normal text-gray-400 '>Log in di bawah untuk akses akun mu</p>
                         </div>
-                        <form action="" className='flex flex-col gap-6'>
-                            <InputLabel name='Nama' placeholder='Masukan Nama' />
-                            <InputLabel type='email' name='Email Amikom' placeholder='Masukan Email' />
-                            <InputLabel type='number' name='NIM' placeholder='Masukan NIM' />
-                            <InputLabel type='password' name='Password' placeholder='Masukan Password' />
-                            <InputLabel type='password' name='Konfirmasi Password ' placeholder='Masukan Kofirmasi Password' />
+                        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-6'>
+                            <InputLabel placeholder='Masukan Nama'  {...register("name")} />
+                            <InputLabel type='email' names='Email Amikom' placeholder='Masukan Email' {...register("email")} />
+                            <InputLabel type='number' names='NIM' placeholder='Masukan NIM' {...register("nim")} />
+                            <InputLabel type='password' names='Password' placeholder='Masukan Password' {...register("password")}  />
+                            <InputLabel type='password' names='Konfirmasi Password ' placeholder='Masukan Kofirmasi Password' {...register("confirm-password")} />
                             <button className='bg-primary-main text-white rounded-lg py-[14px]'>Buat Akun Baru</button>
                             <div className='text-gray-400 text-center'>Sudah punya Akun? <span className='text-primary-main'>Masuk</span></div>
                         </form>
