445db4e82bb9a932f741f847f328b180bd942caa
Muhammad Nizar Fazari <nexsel32@gmail.com>
2024-02-11 19:57:38 +0700

    fixing button in tentukan username, and make choose username page

---
+++ pages/register/choose-profil.tsx
@@ -0,0 +1,13 @@
+import RegisterLayout from '@/layouts/RegisterLayout';
+import * as React from 'react';
+
+interface IChooseProfilProps {
+}
+
+const ChooseProfil: React.FunctionComponent<IChooseProfilProps> = (props) => {
+    return (
+        <></>
+    );
+};
+ChooseProfil.layout = RegisterLayout
+export default ChooseProfil;
--- pages/register/choose-username.tsx
+++ pages/register/choose-username.tsx
@@ -1,6 +1,7 @@
 
 import InputLabel from '@/components/inputs/input-label';
 import RegisterLayout from '@/layouts/RegisterLayout';
+import Link from 'next/link';
 import * as React from 'react';
 
 interface IChooseUsernameProps {
@@ -11,7 +12,7 @@ const ChooseUsername: React.FunctionComponent<IChooseUsernameProps> = (props) =>
   return (
     <>
       <InputLabel type='text' names='Username' placeholder='@johndoe'  />
-      <button className='bg-primary-main text-white rounded-lg w-full mt-6 py-[14px]'>Selanjutnya</button> 
+      <Link href={'/register/choose-profil'} className='bg-primary-main text-white text-center rounded-lg w-full mt-6 py-[14px] font-semibold text-base'>Selanjutnya</Link> 
     </>
   );
 };
