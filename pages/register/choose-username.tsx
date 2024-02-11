cf26ee777cc929317aff9d7f8ffe51ec3c28ad4d
Muhammad Nizar Fazari <nexsel32@gmail.com>
2024-02-11 19:41:54 +0700

    Make Component Stepper and Register Layout

---
+++ pages/register/choose-username.tsx
@@ -0,0 +1,21 @@
+
+import InputLabel from '@/components/inputs/input-label';
+import RegisterLayout from '@/layouts/RegisterLayout';
+import * as React from 'react';
+
+interface IChooseUsernameProps {
+}
+
+const ChooseUsername: React.FunctionComponent<IChooseUsernameProps> = (props) => {
+
+  return (
+    <>
+
+    </>
+  );
+};
+
+
+ChooseUsername.layout = RegisterLayout
+
+export default ChooseUsername;
