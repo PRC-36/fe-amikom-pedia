34e36955d7f0bcf488edf9640bd9859085972833
Muhammad Nizar Fazari <nexsel32@gmail.com>
2024-02-24 13:45:35 +0700

    refactoring in layouts and added page login

--- pages/register.tsx
+++ pages/register.tsx
@@ -5,6 +5,8 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
 import CarouselRegister from '@/components/carousel/carousel-register';
+import AuthLayout from '@/layouts/AuthLayout';
+import Link from 'next/link';
 
 
 interface IRegisterProps {
@@ -36,50 +38,45 @@ const Register: React.FunctionComponent<IRegisterProps> = (props) => {
 
     return (
         <>
-            <div className='grid grid-cols-1 lg:grid-cols-2 h-screen'>
-                <div className='col-span-1 hidden lg:block bg-[#533896]  justify-center items-center overflow-hidden'>
-                    <CarouselRegister />
-                </div>
-                <div className='col-span-1 h-full flex justify-center items-center'>
-                    <div className='w-[420px] p-7'>
-                        <div className='text-center mb-6'>
-                            <div className='mb-6 flex justify-center'>
-                                <img src="/logo.png" alt="" />
-                            </div>
-                            <h2 className='text-2xl font-semibold text-text-500 mb-2'>Masuk ke Akun anda</h2>
-                            <p className='text-base font-normal text-gray-400 '>Log in di bawah untuk akses akun mu</p>
-                        </div>
-                        <form
-                            onSubmit={handleSubmit(onSubmit)}
-                            className='flex flex-col gap-6'>
-                            <div>
-                                <InputLabel placeholder='Masukan Nama'  {...register("name")} />
-                                <p className='text-[14px] leading-5 text-danger-100'>{errors.name?.message}</p>
-                            </div>
-                            <div>
-                                <InputLabel type='email' names='Email Amikom' placeholder='Masukan Email' {...register("email")} />
-                                <p className='text-[14px] leading-5 text-danger-100'>{errors.email?.message}</p>
-                            </div>
-                            <div>
-                                <InputLabel type='number' names='NIM' placeholder='Masukan NIM' {...register("nim")} />
-                                <p className='text-[14px] leading-5 text-danger-100'>{errors.nim?.message}</p>
-                            </div>
-                            <div>
-                                <InputLabel type='password' names='Password' placeholder='Masukan Password' {...register("password")} isVisible />
-                                <p className='text-[14px] leading-5 text-danger-100'>{errors.password?.message}</p>
-                            </div>
-                            <div>
-                                <InputLabel type='password' names='Konfirmasi Password ' placeholder='Masukan Kofirmasi Password' {...register("confirm_password")} isVisible />
-                                <p className='text-[14px] leading-5 text-danger-100'>{errors.confirm_password?.message}</p>
-                            </div>
-                            <button type='submit' className='bg-primary-main text-white rounded-lg py-[14px]'>Buat Akun Baru</button>
-                            <div className='text-gray-400 text-center'>Sudah punya Akun? <span className='text-primary-main'>Masuk</span></div>
-                        </form>
+            <div className='w-[420px] p-7'>
+                <div className='text-center mb-6'>
+                    <div className='mb-6 flex justify-center'>
+                        <img src="/logo.png" alt="" />
                     </div>
+                    <h2 className='text-2xl font-semibold text-text-500 mb-2'>Masuk ke Akun anda</h2>
+                    <p className='text-base font-normal text-gray-400 '>Log in di bawah untuk akses akun mu</p>
                 </div>
+                <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className='flex flex-col gap-6'>
+                    <div className='h-[90px]'>
+                        <InputLabel placeholder='Masukan Nama'  {...register("name")} />
+                        <p className='text-[14px] leading-5 text-danger-100'>{errors.name?.message}</p>
+                    </div>
+                    <div className='h-[90px]'>
+                        <InputLabel type='email' names='Email Amikom' placeholder='Masukan Email' {...register("email")} />
+                        <p className='text-[14px] leading-5 text-danger-100'>{errors.email?.message}</p>
+                    </div>
+                    <div className='h-[90px]'>
+                        <InputLabel type='numeric' names='NIM' placeholder='Masukan NIM' {...register("nim")} />
+                        <p className='text-[14px] leading-5 text-danger-100'>{errors.nim?.message}</p>
+                    </div>
+                    <div className='h-[90px]'>
+                        <InputLabel type='password' names='Password' placeholder='Masukan Password' {...register("password")} isVisible />
+                        <p className='text-[14px] leading-5 text-danger-100'>{errors.password?.message}</p>
+                    </div>
+                    <div className='h-[90px]'>
+                        <InputLabel type='password' names='Konfirmasi Password ' placeholder='Masukan Kofirmasi Password' {...register("confirm_password")} isVisible />
+                        <p className='text-[14px] leading-5 text-danger-100'>{errors.confirm_password?.message}</p>
+                    </div>
+                    <button type='submit' className='bg-primary-main text-white rounded-lg py-[14px]'>Buat Akun Baru</button>
+                    <div className='text-gray-400 text-center'>Sudah punya Akun? <Link href={'/login'} className='text-primary-main'>Masuk</Link></div>
+                </form>
             </div>
         </>
     );
 };
+
+Register.layout = AuthLayout
 
 export default Register;
